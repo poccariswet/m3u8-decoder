ed1fc092d0427ac6de92bc7623dd46e24e3fe675
poccariswet <s1240231@gmail.com>
2019-01-16 22:48:09 +0900

    [ADD] decode from m3u8 file buf

--- decoder.go
+++ decoder.go
@@ -1,5 +1,21 @@
 package m3u8
 
-func DecodeFrom() {
+import (
+	"bytes"
+	"io"
+)
 
+type PlayList struct{}
+
+type PlayListType uint
+
+// Decode From returns playlist and find the type.
+func DecodeFrom(r io.Reader) (PlayList, PlayListType, error) {
+	buf := new(bytes.Buffer)
+	_, err := buf.ReadFrom(r)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return decode(buf)
 }
