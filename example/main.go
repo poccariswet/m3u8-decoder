e98bc843d09fd51a75173261b455189b13c0a2c8
poccariswet <s1240231@gmail.com>
2019-03-24 05:08:58 +0900

    [ADD] example code

--- example/main.go
+++ example/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
 
+	"github.com/poccariswet/m3u8"
+)
+
+func main() {
+	file, err := os.Open("sample path")
+	if err != nil {
+		log.Fataln(err)
+	}
+
+	playlist, listtype, err := m3u8.DecodeFrom(bufio.NewReader(file))
+	if err != nil {
+		log.Fataln(err)
+	}
+
+	fmt.Println(playlist)
+	fmt.Println(listtype)
 }
