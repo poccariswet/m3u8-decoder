f7da5413a7ff54f84ad2bb10d5f2b4b75dca35b9
poccariswet <s1240231@gmail.com>
2019-03-29 17:05:03 +0900

    [FIX] fixed some error

--- decoder.go
+++ decoder.go
@@ -7,18 +7,21 @@ import (
 	"strings"
 )
 
-func (p *MasterPlaylist) Init() error {
-	p.version = uint8(3) // most min version
-	return nil
+func NewMasterPlaylist() *MasterPlaylist {
+	master := new(MasterPlaylist)
+	master.version = uint8(3) // most min version
+
+	return master
 }
 
-func (p *MediaPlaylist) Init() error {
-	p.version = uint8(3)
-	p.capacity = 1024
-	p.playlistSize = 8
-	p.MediaSeqments = make([]*MediaSeqment, p.capacity)
+func NewMediaPlaylist() *MediaPlaylist {
+	media := new(MediaPlaylist)
+	media.version = uint8(3) // most min version
+	media.capacity = 1024
+	media.playlistSize = 8
+	media.MediaSeqments = make([]*MediaSeqment, media.capacity)
 
-	return nil
+	return media
 }
 
 func (p *MasterPlaylist) DecodeFrom(r io.Reader) error {
@@ -38,19 +41,11 @@ func (p *MediaPlaylist) String() string {
 }
 
 func decode(buf *bytes.Buffer) (Playlist, ListType, error) {
-	var master *MasterPlaylist
-	var media *MediaPlaylist
+	master := NewMasterPlaylist()
+	media := NewMediaPlaylist()
 	var listtype ListType
 	var end bool
-	var states *States
-
-	if err := master.Init(); err != nil {
-		return nil, ERRTYPE, err
-	}
-
-	if err := media.Init(); err != nil {
-		return nil, ERRTYPE, err
-	}
+	states := new(States)
 
 	for !end {
 		line, err := buf.ReadString('\n')
@@ -81,14 +76,14 @@ func decode(buf *bytes.Buffer) (Playlist, ListType, error) {
 		return media, listtype, nil
 	}
 
-	return nil, 2, errors.New("not playlist")
+	return nil, ERRTYPE, errors.New("not playlist")
 }
 
 func DecodeFrom(r io.Reader) (Playlist, ListType, error) {
 	buf := new(bytes.Buffer)
 	_, err := buf.ReadFrom(r)
 	if err != nil {
-		return nil, 2, err
+		return nil, ERRTYPE, err
 	}
 	return decode(buf)
 }
