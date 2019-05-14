21366174796d7c658ac3dd96711c3f998f7409cd
poccariswet <s1240231@gmail.com>
2019-05-14 23:08:26 +0900

    [FIX] test master media

--- example/main.go
+++ example/main.go
@@ -3,21 +3,41 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/poccariswet/m3u8-decoder"
 )
 
 func main() {
-	file, err := os.Open("./media.m3u8")
+	media, err := os.Open("./media.m3u8")
 	if err != nil {
-		log.Fatalln(err)
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	master, err := os.Open("./master.m3u8")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 
-	playlist, listtype, err := m3u8.DecodeFrom(bufio.NewReader(file))
+	playlist, listtype, err := m3u8.DecodeFrom(bufio.NewReader(media))
 	if err != nil {
-		log.Fatalln(err)
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	switch listtype {
+	case m3u8.MASTER:
+		fmt.Println(playlist)
+	case m3u8.MEDIA:
+		fmt.Println(playlist)
+	default:
+	}
+
+	playlist, listtype, err = m3u8.DecodeFrom(bufio.NewReader(master))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 
 	switch listtype {
