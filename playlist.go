812099cda9a6d633d7b8e190b10a2fae7886292d
poccariswet <s1240231@gmail.com>
2019-05-18 17:07:35 +0900

    [FIX] playlist

---
+++ playlist.go
@@ -0,0 +1,21 @@
+package m3u8
+
+func NewPlaylist() *Playlist {
+	pl := new(Playlist)
+	pl.version = uint8(3)
+	pl.TargetDuration = 10
+	pl.live = true
+
+	return pl
+}
+
+func (p *Playlist) String() string {
+	if p.master {
+		return "MASTER PLAYLIST"
+	}
+	return "MEDIA PLAYLIST"
+}
+
+func (p *Playlist) AppendSegment(pseg PlaylistSegment) {
+	p.Segments = append(p.Segments, pseg)
+}
