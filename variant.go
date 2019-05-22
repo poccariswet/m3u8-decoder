8a4bb975670c0b507511a0b3f3f2fe4a8ec6b661
poccariswet <s1240231@gmail.com>
2019-05-22 22:36:02 +0900

    [ADD] variant parse

--- variant.go
+++ variant.go
@@ -1,36 +1,69 @@
 package m3u8
 
+import "github.com/pkg/errors"
+
 // NewVariant parse line has EXT-X-STREAM-INF or EXT-X-I-FRAME-STREAM-INF
 func NewVariant(line string) (*VariantAttributes, error) {
 	item := parseLine(line[len(ExtFrameStreamInf+":"):])
 	/*
-		type VariantAttributes struct {
-			URI        string
-			Name       string
-			Subtitle   string
-			Bandwidth  uint64
-			ProgramID  uint64
-			Codec      string
-			AudioCodec string
-			Resolution string
-			Audio      string
-			Video      string
-			FrameRate  float64
-			HDCPLevel  string
-			Resolution *Resolution
+			type VariantAttributes struct {
+				URI              string
+				Name             string
+				Subtitle         string
+				Bandwidth        uint32
+				AverageBandwidth uint32
+				ProgramID        uint32
+				Codec            string
+				AudioCodec       string
+				Audio            string
+				Video            string
+				FrameRate        float64
+				HDCPLevel        string
+				Resolution       *Resolution
 
-			IFrame bool
+				IFrame bool
 		}
 	*/
 
-	resolution, err := NewResolution(item[RESOLUTION])
-	if !has {
-		resolution = nil
+	resolution, err := NewResolution(item, RESOLUTION)
+	if err != nil {
+		return nil, errors.Wrap(err, "resolution parse err: ")
+	}
+
+	bandwidth, err := extractUint64(item, BANDWIDTH)
+	if err != nil {
+		return nil, errors.Wrap(err, "extractUint64 err: ")
+	}
+
+	averageBandwidth, err := extractUint64(item, AVERAGEBANDWIDTH)
+	if err != nil {
+		return nil, errors.Wrap(err, "extractUint64 err: ")
+	}
+
+	programID, err := extractUint64(item, PROGRAMID)
+	if err != nil {
+		return nil, errors.Wrap(err, "extractUint64 err: ")
+	}
+
+	frameRate, err := extractFloat64(item, FRAMERATE)
+	if err != nil {
+		return nil, errors.Wrap(err, "extractFloat64 err: ")
 	}
 
 	return &VariantAttributes{
-		Resolution: resolution,
-	}
+		URI:              item[URI],
+		Name:             item[NAME],
+		Subtitle:         item[SUBTITLES],
+		Bandwidth:        uint32(bandwidth),
+		AverageBandwidth: uint32(averageBandwidth),
+		ProgramID:        uint32(programID),
+		Codec:            item[CODECS],
+		Audio:            item[AUDIO],
+		Video:            item[VIDEO],
+		FrameRate:        frameRate,
+		HDCPLevel:        item[HDCPLEVEL],
+		Resolution:       resolution,
+	}, nil
 }
 
 func (va *VariantAttributes) String() string {
