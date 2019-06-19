e48f01b1655b6b2be026d029b987b8f4943e1b80
poccariswet <s1240231@gmail.com>
2019-06-19 22:13:08 +0900

    [ADD] decode server control tag

--- control.go
+++ control.go
@@ -1,5 +1,7 @@
 package m3u8
 
+import "github.com/pkg/errors"
+
 // ServerControl indicate support for features such as Blocking Playlist Reload and Playlist Delta Updates
 type ServerControlSegment struct {
 	CanBlockReload bool
@@ -9,8 +11,35 @@ type ServerControlSegment struct {
 }
 
 // New server control segment
-func NewServerControl() (*ServerControlSegment, error) {
-	return &ServerControlSegment{}, nil
+func NewServerControl(line string) (*ServerControlSegment, error) {
+	item := parseLine(line[len(ExtServerControl+":"):])
+
+	canblock, err := extractBool(item, CANBLOCKRELOAD)
+	if err != nil {
+		return nil, errors.Wrap(err, "extractBool err")
+	}
+
+	until, err := extractFloat64(item, CANSKIPUNTIL)
+	if err != nil {
+		return nil, errors.Wrap(err, "extractFloat64 err")
+	}
+
+	holdback, err := extractFloat64(item, HOLDBACK)
+	if err != nil {
+		return nil, errors.Wrap(err, "extractFloat64 err")
+	}
+
+	parthb, err := extractFloat64(item, PARTHOLDBACK)
+	if err != nil {
+		return nil, errors.Wrap(err, "extractFloat64 err")
+	}
+
+	return &ServerControlSegment{
+		CanBlockReload: canblock,
+		CanSkipUntil:   until,
+		HoldBack:       holdback,
+		PartHoldBack:   parthb,
+	}, nil
 }
 
 // segment to string
--- control_test.go
+++ control_test.go
@@ -1,7 +1,23 @@
 package m3u8_test
 
-import "testing"
+import (
+	"testing"
+
+	m3u8 "github.com/poccariswet/m3u8-decoder"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestServerContorol(t *testing.T) {
+	line := `#EXT-X-SERVER-CONTROL:CAN-BLOCK-RELOAD=YES,
+PART-HOLD-BACK=1.0,CAN-SKIP-UNTIL=12.0,HOLD-BACK=6.0`
 
+	sc, err := m3u8.NewServerControl(line)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.True(t, sc.CanBlockReload)
+	assert.Equal(t, float64(12.0), sc.CanSkipUntil)
+	assert.Equal(t, float64(6.0), sc.HoldBack)
+	assert.Equal(t, float64(1.0), sc.PartHoldBack)
 }
