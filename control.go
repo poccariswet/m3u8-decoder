7faa70b4744f9a8c9c6a3f7297d2096fc2b3d273
poccariswet <s1240231@gmail.com>
2019-06-19 21:05:03 +0900

    [ADD] server control segment

--- control.go
+++ control.go
@@ -1,11 +1,19 @@
 package m3u8
 
-type ServerControlSegment struct{}
+// ServerControl indicate support for features such as Blocking Playlist Reload and Playlist Delta Updates
+type ServerControlSegment struct {
+	CanBlockReload bool
+	CanSkipUntil   float64
+	HoldBack       float64
+	PartHoldBack   float64
+}
 
+// New server control segment
 func NewServerControl() (*ServerControlSegment, error) {
 	return &ServerControlSegment{}, nil
 }
 
+// segment to string
 func (ss *ServerControlSegment) String() string {
 	return "ServerControlSegment"
 }
